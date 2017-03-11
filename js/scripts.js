fe74dbb569e6978550f07f3363e176d23d9be54f
Justa <filjust@gmail.com>
2017-03-11 18:37:06 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('span:odd').css('color', 'red');
+
+    var paragraphs = $('p');
+    paragraphs.each(function(index, element) {
+
+        var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+        $(element).append(button);
+
+
+    });
+    $("button").click(function() {
+	        alert($(this).attr("data-tmp"));
+    });
+    var buttons = $('button');
+    buttons.each(function() {
+        $('button').css('background', 'white');
+    });
+});
